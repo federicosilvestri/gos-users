19b10dec2717374660a7afbf613d216d1ab47234
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2020-11-17 12:01:47 +0100

    added example to load authority

---
+++ load_example_data.py
@@ -0,0 +1,33 @@
+import json
+
+from werkzeug.security import generate_password_hash
+
+import gooutsafe
+
+PATH_HEALTH_AUTH_DATA = 'example_data/health_authority.json'
+
+
+def load_health_auth_data():
+    with open(PATH_HEALTH_AUTH_DATA) as json_file:
+        dict_health_auth = json.load(json_file)
+
+    email = dict_health_auth["email"]
+    password = generate_password_hash(dict_health_auth["password"])
+    name = dict_health_auth["name"]
+    city = dict_health_auth["city"]
+    address = dict_health_auth["address"]
+    phone = dict_health_auth["phone"]
+
+    from gooutsafe.models import health_authority
+    lha = health_authority.Authority(email=email, password=password, name=name,
+                                     city=city,
+                                     address=address, phone=phone
+                                     )
+
+    from gooutsafe.dao.health_authority_manager import AuthorityManager
+    AuthorityManager.create_authority(lha)
+
+
+if __name__ == "__main__":
+    gooutsafe.create_app()
+    load_health_auth_data()
